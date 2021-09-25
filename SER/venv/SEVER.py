2a9dc0f7f78d0df0b1bdb6fc65efabd329191170
valery <valeradoroshkov987@gmail.com>
2021-09-25 22:41:31 +0300

    Удалил

--- SER/venv/SEVER.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello PrettyGuy!'
--- pythonProject/SERVER.py
+++ pythonProject/SERVER.py
@@ -1,6 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello, Valery"
+
+
+@app.route('/about')
+def about():
+    return "About us"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
