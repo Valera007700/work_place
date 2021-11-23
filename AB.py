fb446ddfb5b8130d2e2f75549fc88430a028e65f
valery <valeradoroshkov987@gmail.com>
2021-11-23 12:30:48 +0300

    Классы

--- AB.py
+++ AB.py
@@ -1,3 +1,36 @@
-a = 17
-b = True if a > 10 else False
-print(b)
+
+
+#class Point:
+#    color = "red"
+#    circle = 23
+
+#print(Point.color, Point.circle)
+#class Point:
+#    color = "green"
+#print(Point.color)
+
+#a = 17
+#b = True if a > 10 else False
+#print(b, Point.color)
+
+
+a = 2
+while a<5:
+    print(a, "Это до")
+    a = a+1
+    print(a, "----Это после")
+else:
+    print("NICE")
+
+#_____________
+
+class Pount:
+    "Класс для представления координат на полокости "
+    color = "red"
+    circle = 2
+
+##<><><><><><>
+Point.color = "black" - меняем атрибут в классе 
+Point.__dict__ - пказывает все атрибуты в классе Point
+getattr(point, "color", False)
+Point.__doc__ - То, что написано в комментакх после определения класса. 
